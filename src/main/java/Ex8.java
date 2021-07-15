822f8631cdb6b1d76d318d3d829eb80436011dcd
SUNGJUNGHOON <87322099+SUNGJUNGHOON@users.noreply.github.com>
2021-07-15 13:53:53 +0900

    Add files via upload

--- src/main/java/Ex8.java
+++ src/main/java/Ex8.java
@@ -1,2 +1,12 @@
 public class Ex8 {
+    // TODO : 프로그램 시작 시 "Hello World!" 를 출력하도록 만들기
+    public static void main(String[] args) {
+        System.out.println("Hello World!"
+        );
+        for (int i = 1; i <= 5; i++) {
+          if (i>2)
+            System.out.print(i);
+
+        }
+    }
 }
